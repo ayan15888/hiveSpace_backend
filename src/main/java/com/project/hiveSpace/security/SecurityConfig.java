fe7083f63757aca8b5a4d18e5d011aeb6d46f6da
Sanjay <sanjaybarman1179@gmail.com>
2026-03-30 18:34:23 +0530

    testing

--- src/main/java/com/project/hiveSpace/security/SecurityConfig.java
+++ src/main/java/com/project/hiveSpace/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.hiveSpace.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +31,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/register", "/login").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/tenants/**").permitAll()
